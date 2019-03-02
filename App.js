e7e1cf302ae4388a4d04c71c06db50fcfbbf807d
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2019-03-02 19:32:31 +0000

    Create NoiseHistory components to test the database

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import NoiseHistory from './src/components/NoiseHistory/NoiseHistory';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,12 +20,14 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <NoiseHistory/>
       </View>
     );
   }
---
+++ src/components/NoiseHistory/NoiseHistory.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { View, FlatList, Text, StyleSheet} from 'react-native';
+import {queryAllNoise, insertNoise} from '../../database/schemas';
+import realm from '../../database/schemas';
+
+class NoiseHistory extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            noiseList: []
+        }
+    }
+
+    componentWillMount() {
+        // insertNoise({
+        //     id: 2,
+        //     level: 60.5,
+        //     timestamp: new Date(),
+        //     longitude: 1000034.34,
+        //     latitude: 200034.454,
+        //     // type: ''
+        //     deviceModel: 'FJ3453',
+        //     isPublic: false,
+        //     isSynced: false
+        // }).then(value => this.reloadData()).catch(error => {
+        //     console.log(error)
+        // })
+        this.reloadData();
+    }
+
+    reloadData = () => {
+        queryAllNoise().then((noiseList) => {
+            console.log(noiseList[0].level);
+            this.setState({noiseList})
+        }).catch(error => {
+            console.log("error in reloading noise history list", error);
+            this.state({noiseList: []})
+        });
+        console.log('reloadData')
+    };
+
+    render() {
+        // if (this.state.noiseList)
+        //     console.log(this.state.noiseList[0].level);
+        return (<View>
+            <Text>Noise History</Text>
+            {/*<FlatList*/}
+                {/*style={styles.flatList}*/}
+                {/*data={this.state.noiseList}*/}
+                {/*renderItem={} />*/}
+        </View>);
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start'
+    },
+    flatList: {
+        flex: 1,
+        flexDirection: 'column',
+    }
+})
+
+export default NoiseHistory
