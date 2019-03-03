4fc9ec5d84b3b0eab7cf3d346adb5f02f4673d26
Ieuan Parker <Parkeri5@cardiff.ac.uk>
2019-03-03 20:40:20 +0000

    Added a navigation bar that displays on phone and has cardiff's colours

--- src/components/NavigationMenu/NavigationMenu.js
+++ src/components/NavigationMenu/NavigationMenu.js
@@ -1,64 +1,34 @@
-import React from 'react'
-import { Button, Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import {
-    createStackNavigator,
-    createBottomTabNavigator,
-    createAppContainer,
-} from 'react-navigation';
-
-import HomeNoise from '../.,/pages/HomeNoise';
-import SettingsScreen from '../../pages/SettingsScreen';
-import DetailsScreen from '../../pages/DetailsScreen';
-import HistoryScreen from '../../pages/HistoryScreen';
-
-const HomeStack = createStackNavigator(
-    {
-        Home: { screen: HomeNoise },
-        Details: { screen: DetailsScreen},
-    },
-    {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: '#176381',
-            },
-            headerTintColor: '#FFFFFF',
-            title: 'Home',
-        },
+import React, { Component } from 'react';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
+export default class NavigationMenu extends Component {
+    render() {
+        return (
+            <Container>
+                <Header style={{ backgroundColor: '#176381'}}>
+                </Header>
+                <Content />
+                <Footer>
+                    <FooterTab
+                        style={{ backgroundColor: '#176381'}}>
+                        <Button vertical >
+                            <Icon name="ios-volume-high" />
+                            <Text>Noise</Text>
+                        </Button>
+                        <Button vertical active style={{backgroundColor: '#176381'}}>
+                            <Icon active name="ios-create" />
+                            <Text>Add Details</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="ios-time" />
+                            <Text>History</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="ios-settings" />
+                            <Text>Settings</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
+        );
     }
-);
-
-const App = createBottomTabNavigator(
-    {
-        Home: { screen : HomeNoise},
-        Details: { screen: DetailsScreen},
-        History: { screen: HistoryScreen},
-        Settings: { screen: SettingsScreen}
-    },
-    {
-        defaultNavigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, horizontal, tintColor}) => {
-                const {routeName} = navigation.state;
-                let IconComponent = Ionicons;
-                let iconName;
-                if (routeName === 'Home') {
-                    iconName = `ios-volume-high${focused ? '' : '-outline'}`;
-                } else if (routeName === 'Details') {
-                    iconName = `material-create${focused ? '' : '-outline'}`;
-                } else if (routeName === 'History') {
-                    iconName = `material-time${focused ? '' : '-outline'}`;
-                } else if (routeName === 'Home') {
-                    iconName = `ios-settings${focused ? '' : '-outline'}`;
-                }
-                return <IconComponent name={iconName} size={25} color={tintColor}/>;
-            }
-        }),
-        tabBarOptions:{
-            activeTintColor: '#FFFFFF',
-            inactiveTintColor: 'gray'
-        }
-    }
-);
-
-export default createAppContainer(App)
+}
