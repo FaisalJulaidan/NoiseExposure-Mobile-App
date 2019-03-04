381970c5211c1c1bed8e6232a8278a100e6899fe
Ieuan Parker <iparker.pjstudios@gmail.com>
2019-03-04 16:15:47 +0000

    Added route details for navigation

--- src/components/NavigationMenu/NavigationMenu.js
+++ src/components/NavigationMenu/NavigationMenu.js
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
-export default class NavigationMenu extends Component {
-    render() {
+import HomeScreen from '../../screens/HomeScreen.js';
+import HistoryScreen from '../../screens/HistoryScreen.js';
+import SettingsScreen from '../../screens/SettingsScreen.js';
+import DetailsScreen from '../../screens/DetailsScreen.js';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+
+
+const RootStack = createStackNavigator(
+    {
+        Noise: HomeScreen,
+        Details: DetailsScreen,
+        History: HistoryScreen,
+        Settings: SettingsScreen
+    },
+    {
+        initialRouteName: 'Noise'
+    });
+
+const AppContainer = createAppContainer(RootStack);
+
+
+class NavigationMenu extends Component{
+    render(){
         return (
-            <Container>
-                <Header style={{ backgroundColor: '#176381'}}>
-                </Header>
-                <Content />
                 <Footer>
                     <FooterTab
                         style={{ backgroundColor: '#176381'}}>
-                        <Button vertical >
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.navigate('Noise')}>
                             <Icon name="ios-volume-high" />
                             <Text>Noise</Text>
                         </Button>
-                        <Button vertical active style={{backgroundColor: '#176381'}}>
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.navigate('Details')}>
                             <Icon active name="ios-create" />
                             <Text>Add Details</Text>
                         </Button>
-                        <Button vertical>
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.navigate('History')}>
                             <Icon name="ios-time" />
                             <Text>History</Text>
                         </Button>
-                        <Button vertical>
+                        <Button
+                            vertical
+                            onPress={() => this.props.navigation.navigate('Settings')}>
                             <Icon name="ios-settings" />
                             <Text>Settings</Text>
                         </Button>
                     </FooterTab>
                 </Footer>
-            </Container>
         );
     }
 }
+
+export default NavigationMenu(AppContainer)
