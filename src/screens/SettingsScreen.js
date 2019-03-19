aeae78a280f6e2653a399c1ea0643abfcab64a71
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2019-03-19 17:20:27 +0000

    Created Toggle in Settings to change Map theme

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,36 +1,85 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Icon, Text, View } from 'native-base';
-import { Button } from 'react-native';
-import {sendNoiseDataToServer} from "../utilities";
+import { Text, View, Switch } from 'native-base';
+import {Button} from 'react-native';
+import {MAP_THEME_KEY, retrieveData, sendNoiseDataToServer, storeData} from "../utilities";
 
-class SettingsScreen extends React.Component {
+class SettingsScreen extends Component {
+
+    state = {
+        darkThemeToggle: false,
+        asyncStorageNotLoaded: true
+    };
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            noise: "_"
-        };
+        // Call the Async Storage to get MapThemeKey Value
+        retrieveData(MAP_THEME_KEY).then((value) => {
+            if (value === 'dark') {
+                this.setState( {
+                    darkThemeToggle: true,
+                    asyncStorageNotLoaded: false
+                });
+            } else if (value === 'light') {
+                this.setState( {
+                    darkThemeToggle: false,
+                    asyncStorageNotLoaded: false
+                });
+            }
+        }).catch((error) => {
+            console.log(error);
+            this.setState( {
+                asyncStorageNotLoaded: true
+            });
+        });
     }
+
+    // Method to set the Map theme in local storage
+    changeMapTheme = (value) => {
+        // Set Value in state
+        this.setState({
+
+            darkThemeToggle: value
+        });
+
+        let mapTheme;
+
+        if (value === true) {
+            mapTheme = 'dark';
+            console.log("theme toggle to dark");
+
+        } else if (value === false) {
+            mapTheme = 'light';
+            console.log("theme toggle to light");
+        }
+
+        storeData(MAP_THEME_KEY, mapTheme).then((value) => {
+
+        }).catch((error) => {
+
+        });
+    };
 
 
     sendDataToServer = () => {
-        const response = sendNoiseDataToServer();
-
-        this.state = {
-            noise: response
-        }
+        sendNoiseDataToServer();
     };
 
     render() {
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text> {this.state.noise} </Text>
+                <Text> {this.state.darkThemeToggle.valueOf()} </Text>
                 <Text>Settings SCREEN</Text>
                 <Button
-                    onPress={this.sendDataToServer}
+                    onPress={this.sendDataToServer()}
                     title="Sync Data"
                     disabled={false}
+                />
+                <Text>Enable Dark Theme </Text>
+                <Switch
+                    onValueChange = {this.changeMapTheme}
+                    value = {this.state.darkThemeToggle}
+                    disabled={this.state.asyncStorageNotLoaded}
                 />
             </View>
         );
