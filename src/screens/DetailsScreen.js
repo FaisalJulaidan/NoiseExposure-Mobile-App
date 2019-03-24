c5e7680987665081f015f02f0f3e804f575d0ecc
Joey Gmaj <joeygmaj@hotmail.co.uk>
2019-03-24 13:58:34 +0000

    form added to additional details page, with an option to choose predefined options for the noise type

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text, View } from 'native-base';
+import {StyleSheet, TextInput, Text, View, Picker } from 'react-native';
 
-class DetailsScreen extends React.Component {
+class DetailsScreen extends Component {
+    state = {
+        type: '',
+        descriptions: ''
+    };
+
+    updateType = (type) => {
+        this.setState({type : type })
+    };
+    
     render() {
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <View>
                 <Text>Details SCREEN</Text>
+                <Text>Here you can enter information relating to a specific noise level.</Text>
+
+                <Text>Noise Type: </Text>
+                <Picker
+                    selectedValue={this.state.type}
+                    onValueChange={this.updateType}>
+                    <Picker.item
+                    label = "Construction"
+                    value = 'construction' />
+                    <Picker.item
+                    label = "Sports Event"
+                    value = 'sports' />
+                    <Picker.item
+                    label = "People"
+                    value = 'people' />
+                    <Picker.item
+                    label = "Other"
+                    value = 'other' />
+                </Picker>
+                <Text>Additional Details: </Text>
+                <View style={styles.addDetails}>
+                    <TextInput
+                        onChangeText={(descriptions) => this.setState({descriptions})}
+                        value={this.state.descriptions}
+                        placeholder={"Enter Additional Information Here"}
+                        maxLength = {200}>
+                    </ TextInput>
+                </View>
             </View>
         );
     }
 }
-
 export default DetailsScreen
+
+const styles = StyleSheet.create({
+    addDetails: {
+        borderRadius: 4,
+        borderColor: "#018a99"
+    }
+})
