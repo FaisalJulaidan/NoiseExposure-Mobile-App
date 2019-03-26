5de8786a1a82b09f8bf70cde8ce2e9ce93d865cc
Ieuan Parker <Parkeri5@cardiff.ac.uk>
2019-03-26 17:05:43 +0000

    Changed colour scheme of buttons so they stand out more

--- native-base-theme/variables/platform.js
+++ native-base-theme/variables/platform.js
@@ -105,7 +105,7 @@ export default {
   checkboxTickColor: "#fff",
 
   // Color
-  brandPrimary: platform === "ios" ? "#018a99" : "#018a99",
+  brandPrimary: platform === "ios" ? "#015b66" : "#015b66",
   brandInfo: "#62B1F6",
   brandSuccess: "#5cb85c",
   brandDanger: "#d9534f",
