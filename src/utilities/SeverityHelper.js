6be0be6a8bacd60590d499fe147508f5cd8fff21
Matthew Palmer <38406765+matthewandrewpalmer@users.noreply.github.com>
2019-04-01 22:43:27 +0100

    Added Severity Helper in Utilities

---
+++ src/utilities/SeverityHelper.js
@@ -0,0 +1,21 @@
+export const severitySet = (noiseLevel) => {
+    if(noiseLevel < 71 ){
+        return {
+            severityNo: 1,
+            severityColour: '#1AB518',
+            severityName: 'Normal'
+        }
+    } else if(noiseLevel > 69 && noiseLevel < 111 ){
+        return {
+            severityNo: 2,
+            severityColour: '#FFE400',
+            severityName: 'Warning'
+        }
+    } else if(noiseLevel > 110 ){
+        return {
+            severityNo: 3,
+            severityColour: '#FF0000',
+            severityName: 'Dangerous'
+        }
+    }
+};
